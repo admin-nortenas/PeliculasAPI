d613b4ad2d6f596c6f79a006ea530e30a4f36a72
Maria Angelica Zermeño Martinez <angelicaz@nortenas.com.mx>
2022-01-03 10:16:11 -0700

    se agregó el cambio

---
+++ PelicuasAPI/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PelicuasAPI.Entidades;
+
+namespace PelicuasAPI
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Genero> Generos { get; set; }
+    }
+
+    
+}
---
+++ PelicuasAPI/Startup.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PelicuasAPI
+{
+    public class Startup
+    {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
+
+            services.AddControllers();
+
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+
+
+
+        }
+
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting()
+;
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+
+            {
+                endpoints.MapControllers();
+
+            });
+        }
+    }
+}
